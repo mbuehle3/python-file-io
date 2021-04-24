9490cc2fccaf29e7903dd013f1bc93b5f398c5ae
mbuehle3 <mbuehle3@gmail.com>
2021-04-24 18:01:54 -0500

    project completed

--- origin_parse.py
+++ origin_parse.py
@@ -1,41 +1,35 @@
 import sys
+import re
 
-def read_text(file):
-    """
-    Use function to read in a user defined text file
-    """
-    with open(file, 'r') as f:
-        text = f.readlines()
+def search(regex, doc, output = 'output.txt'):
+    '''
+    A function to search a file some matching regular expression
 
-    return text
+    INPUT 
+    -------
+    regex - an object that contains a compiled regular expression command 
+    e.g, r = re.compile(r'\bherit+\w*\b | \binherit+\w*\b', flags=re.I | re.X)
+    ------
+    doc - a text file to be read in, needs to be provided as a string
+    e.g, 'path/to/your/file.txt'
+    ------
+    output - a string containing the name of the output file
+    e.g., 'output.txt'
+    '''
+    with open(doc, 'r') as in_stream:
+        print('Parsing file',doc)
+        with open(output, 'w') as out_stream:
+            for line_num, line in enumerate(in_stream, 1):
+                if regex.findall(line):
+                    match = regex.search(line)
+                    # print(match.group(0))
+                    # print(match.group())
+                    out_stream.write("{0}\t{1}\n".format(line_num, match.group()))
+    print("Complete, information in",output)
 
-def line_count(file):
-    """
-    Use this function to calculate the total number of lines in a file.
-    INPUT:
-    -----
-    file = str of file to be processed
-    """
-    read_text(file)
-    line_count = 0
-    file = read_text(file)
-    for line in file:
-        line = line.strip("\n")
-        line_count += 1
-        print(line_count,':', line) # Can be used to print all lines in human readable format
-    return line_count
-
-def by_line(txt_obj):
-    for line in txt_obj:
-        line = line.strip("\n")
-        line = print(line)
-    return line
-
-# def word_find(file, regex_pattern):
-    
-# print(s)
 
 if __name__ == '__main__':
-    if len(sys.argv)!= 2:
-        sys.exit(sys.argv[0] + ": Expecting you to give me a text file to parse")
-    text = open(sys.argv[1],'r')
+    regex = re.compile(r'\bherit+\w*\b | \binherit+\w*\b', flags=re.I | re.X)
+    doc = 'origin.txt'
+    output = 'results.txt'
+    search(regex,doc,output)
